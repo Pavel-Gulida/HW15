0411abd853d4835fbfbaf77e88321cfeb5c07458
E6470 <pavel.gulida@gmail.com>
2024-06-28 18:08:28 +0300

    Initial Commit

---
+++ HW15.1.py
@@ -0,0 +1,40 @@
+import random
+class Rectangle:
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_square(self):
+        return self.width * self.height
+
+    def __eq__(self, other):
+        if self.get_square() == other.get_square():
+            return True
+        return False
+    def __add__(self, other):
+        square = self.get_square() + other.get_square()
+        a = random.randint(1, square)
+        return Rectangle(square / a, a)
+
+    def __mul__(self, n):
+        return Rectangle(self.width * n, self.height)
+
+    def __str__(self):
+        pass
+
+
+r1 = Rectangle(2, 4)
+r2 = Rectangle(3, 6)
+assert r1.get_square() == 8, 'Test1'
+assert r2.get_square() == 18, 'Test2'
+
+r3 = r1 + r2
+assert r3.get_square() == 26, 'Test3'
+
+r4 = r1 * 4
+assert r4.get_square() == 32, 'Test4'
+
+assert Rectangle(3, 6) == Rectangle(2, 9), 'Test5'
+
+print("OK")
